389a17577b5959ad446723c48bd2ebfd498dc49a
Taha Azab <azab.taha@gmail.com>
2012-11-17 15:30:26 +0200

    Added new console project and added structuremap nuget package

---
+++ IoCUsingStructureMap/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructureMap;
+
+namespace IoCUsingStructureMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
